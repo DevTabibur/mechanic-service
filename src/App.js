77e6a28011dbf2294e791d42f28e64eaf294cc94
unknown <tobiburrohman2@gmail.com>
2022-07-29 21:40:56 +0600

    public route is done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import About from "./pages/About/About";
-import Contact from "./pages/Contact/Contact";
-import Home from "./pages/Home/Home";
-import Services from "./pages/Services/Services";
+import { Route, Routes } from "react-router-dom";
 import Header from "./pages/shared/Header/Header";
+import { publicRoute } from "./routes/publicRoute";
+
+
+
 
 function App() {
   return (
     <>
       <Header>
-        <Home></Home>
-        <About></About>
-        <Services></Services>
-
-        <Contact></Contact>
+        <Routes>
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} /> */}
+          {
+            publicRoute.map( ({path, name, Component}, index) => (<Route key={index} path={path} element={<Component />} />) )
+          }
+        </Routes>
       </Header>
     </>
   );
---
+++ src/routes/publicRoute.js
@@ -0,0 +1,16 @@
+import About from "../pages/About/About";
+import Contact from "../pages/Contact/Contact";
+import Home from "../pages/Home/Home";
+import Services from "../pages/Services/Services";
+import Header from "../pages/shared/Header/Header";
+import Login from '../pages/Login/Login';
+import Register from '../pages/Register/Register';
+
+export const publicRoute = [
+    {path:"/", name:"Home", Component: Home},
+    {path:"/about", name:"About", Component: About},
+    {path:"/services", name:"Services", Component: Services},
+    {path:"/contact", name:"Contact", Component: Contact},
+    {path:"/login", name:"Login", Component: Login},
+    {path:"/register", name:"Register", Component: Register}
+  ]
