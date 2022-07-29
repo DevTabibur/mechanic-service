00e89e8a06b9997892504b0c5a70248e98ad6b6f
unknown <tobiburrohman2@gmail.com>
2022-07-29 08:49:57 +0600

    daisyui installed and worked

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@
 
 function App() {
   return (
-    <h2>Hello</h2>
+    <>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button class="btn">Button</button>
+<button class="btn btn-primary">Button</button>
+<button class="btn btn-secondary">Button</button>
+<button class="btn btn-accent">Button</button>
+<button class="btn btn-ghost">Button</button>
+<button class="btn btn-link">Button</button>
+</>
   );
 }
 
