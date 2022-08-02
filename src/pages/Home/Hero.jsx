8f7ab3c9e7bc55e7b2821076468fedd572b721e7
unknown <tobiburrohman2@gmail.com>
2022-08-02 09:47:45 +0600

    toggle is woking

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -1,104 +1,106 @@
-import React from 'react';
-import './Home.css'
-import BucketGirl from '../../assets/Images/bucketgirl.png'
+import React from "react";
+import "./Home.css";
+import BucketGirl from "../../assets/Images/bucketgirl.png";
 
 const Hero = () => {
   return (
-    <div className='relative'>
-      <div className='hero h-screen lg:h-[60vh] bg-accent relative z-10 mt-16'>
-        <div className='hero-content flex-col lg:flex-row'>
-          <div data-aos="fade-right"
-     data-aos-offset="300"
-     data-aos-easing="ease-in-sine">
+    <div className="relative">
+      <div className="hero h-screen lg:h-[60vh] bg-accent relative z-10 mt-16">
+        <div className="hero-content flex-col lg:flex-row">
+          <div
+          >
             <p
-              data-aos='fade-right'
-              data-aos-duration='1000'
-              data-aos-delay='200'
-              className='text-xl'
+              data-aos="fade-right"
+              data-aos-duration="1000"
+              data-aos-delay="200"
+              className="text-xl"
             >
               Best Quality
             </p>
             <h1
-              data-aos='fade-right'
-              data-aos-delay='400'
-              data-aos-duration='900'
-              className='text-5xl font-bold'
+              data-aos="fade-right"
+              data-aos-delay="400"
+              data-aos-duration="900"
+              className="text-5xl font-bold"
             >
               Professional Cleaning Service
             </h1>
             <p
-              data-aos='fade-right'
-              data-aos-delay='600'
-              data-aos-duration='800'
-              className='py-6 max-w-xl'
+              data-aos="fade-right"
+              data-aos-delay="600"
+              data-aos-duration="800"
+              className="py-6 max-w-xl"
             >
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has
             </p>
             <button
-              data-aos='zoom-in'
-              data-aos-delay='1300'
-              className='btn btn-primary'
+              data-aos="zoom-in"
+              data-aos-delay="1300"
+              className="btn btn-primary"
             >
               Get Started
             </button>
           </div>
-          <div data-aos="fade-left"
-     data-aos-anchor="#example-anchor"
-     data-aos-offset="500"
-     data-aos-duration="500" className='h-[60vh] shrink-0'>
-            <img src={BucketGirl} className='h-full' alt='hero pics' />
+          <div
+            data-aos="fade-left"
+            data-aos-anchor="#example-anchor"
+            data-aos-offset="500"
+            data-aos-duration="500"
+            className="h-[60vh] shrink-0"
+          >
+            <img src={BucketGirl} className="h-full" alt="hero pics" />
           </div>
         </div>
       </div>
-      <div className='rounded-2xl mx-auto mt-[-50px] relative z-20 bg-base-200 shadow-lg p-10 w-5/6'>
-        <h1 className='text-2xl mb-5 text-primary'>Get Free Estimate</h1>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 '>
+      <div className="rounded-2xl mx-auto mt-[-50px] relative z-20 bg-base-200 shadow-lg p-10 w-5/6">
+        <h1 className="text-2xl mb-5 text-primary">Get Free Estimate</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
           <input
-            type='text'
-            placeholder='Type here'
-            className='input input-bordered w-full'
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
           />
           <input
-            type='text'
-            placeholder='Type here'
-            className='input input-bordered w-full'
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
           />
           <input
-            type='text'
-            placeholder='Type here'
-            className='input input-bordered w-full'
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
           />
           <input
-            type='text'
-            placeholder='Type here'
-            className='input input-bordered w-full'
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
           />
           <input
-            type='text'
-            placeholder='Type here'
-            className='input input-bordered w-full'
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
           />
           <input
-            type='text'
-            placeholder='Type here'
-            className='input input-bordered w-full'
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
           />
           <input
-            type='text'
-            placeholder='Type here'
-            className='input input-bordered w-full'
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
           />
           <input
-            type='text'
-            placeholder='Type here'
-            className='input input-bordered w-full'
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
           />
         </div>
-        <button className='btn btn-primary mt-5'>Get a Quote</button>
+        <button className="btn btn-primary mt-5">Get a Quote</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
